3e7aee0439ea013e99fbcfb9f41417e8f5c0dd84
chestalboiron <chestalboiron@gmail.com>
2013-03-06 15:14:25 -0800

    Update mend_pair.c

--- src/mend_pair.c
+++ src/mend_pair.c
@@ -1,12 +1,13 @@
 #include <R.h>
 #include <stdio.h>
+#include <math.h>
 #include "solidmon.h"
 
-#define G(i,j,k,L) i*2*L + j*2 + k
+#define G(L,s,H,nS) (2*nS)*L + s*2 + H 
 
 /*generate all possible pairs between S vec and K vec but skip any pairs that exceed a pre-defined level of mendalian incompatibility */
 
-void Pair_filter(int *max_incompat, int *S, int *K, double *P,  double *excP, int *L, int *nS, int *nK, int *ps)
+int Pair_filter(int *max_incompat, int *S, int *K, double *P,  double *excP, int *L, int *nS, int *nK, int *ps)
 {
 	int num_S = *nS;
 	int num_K = *nK;
@@ -21,39 +22,34 @@ void Pair_filter(int *max_incompat, int *S, int *K, double *P,  double *excP, in
 	//iterate for each dad 
 	for (s = 0; s < num_S; s++) { //iterate for each dad 
 		for (k = 0; k < num_K; k++) { //iterate thru each kids
-			double prob_pair = 1;
-			double unrel_prob = 1;
+			double prob_pair = 0;
+			double unrel_prob = 0;
 			int match = 0;
 			
 			//iterate thru each locus
 			fprintf(out, "%i\t%i\t" ,s+1, k+1);
-			for (j = 0, match=0; j < num_L, match < max_incom ; j++) { //iterate thru each locus
-				a = S[G(s, j, 0, num_L)]; 
-				b = S[G(s, j, 1, num_L)];
-				c = K[G(s, j, 0, num_L)]; 
-				d = K[G(s, j, 1, num_L)];
+			for (j = 0, match=0; j < num_L ; j++) { //iterate thru each locus
+				a = S[G(j, s, 0, num_S)]; 
+				b = S[G(j, s, 1, num_S)];
+				c = K[G(j, k, 0, num_K)]; 
+				d = K[G(j, k, 1, num_K)];
 				double *p =  P + ps[j];
 				
-				if (MISSING) {
-					 prob_pair *= 1;
-					 unrel_prob *= 1;
-					 fprintf(out, "-1\t");
-					 continue;
-				}
+				fprintf(out, "%i\t", (MISSING)*-1 + (!MISSING)*(SHARES_1_OR_2));
+				//fprintf(out, "%i %i %i %i , %i, %i, %i, \n", a, b, c, d, G(s, k, 0, j), G(s, k, 1,j), j);
+				if (MISSING) 
+				    continue;
 				
 				if(SHARES_1_OR_2) {
 					match++;
-					prob_pair *= PO_PROB;
-					unrel_prob *= UN_PROB;
-					fprintf(out, "1\t");
-				}
-				else{
-					fprintf(out, "0\t");
+					prob_pair += log(PO_PROB);
+					unrel_prob += log(UN_PROB);
 				}				
 				
 			}
-			fprintf(out, "%i\t%lf\t$lf\n" ,match, prob_pair, unrel_prob);
+			fprintf(out, "%i\t%lf\t%lf\n" ,match, prob_pair, unrel_prob);
 		}
 	}
 	fclose(out);
+	return 1;
 }
